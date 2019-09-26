049e2b57a4fc3a89cb0fff4cb7f80ff27ebbf276
Khaled Osman <khaled.osmaan@gmail.com>
2019-09-26 15:49:55 +0200

    chore: initial commit/setup

---
+++ server.js
@@ -0,0 +1,38 @@
+'use strict'
+const express = require('express')
+require('dotenv').config()
+const AWS = require('aws-sdk')
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: process.env.AWS_REGION
+})
+const sqs = new AWS.SQS()
+
+const params = {
+  MessageBody: 'Information about current NY Times fiction bestseller for week of 12/11/2016.',
+  // MessageDeduplicationId: "TheWhistler",  // Required for FIFO queues
+  // MessageId: "Group1",  // Required for FIFO queues
+  QueueUrl: process.env.SQS_QUEUE_URL
+}
+
+// Constants
+const HOST = process.env.PORT || 'localhost'
+const PORT = process.env.HOST || 8080
+
+// App
+const app = express()
+app.get('/', (req, res) => {
+  res.send('Hello world\n')
+})
+
+app.listen(`${HOST}:${PORT}`, () => {
+  console.log(`Running on http://${process.env.HOST}:${process.env.PORT}`)
+  sqs.sendMessage(params)
+    .promise()
+    .then((wat) => {
+      console.log(wat)
+    }).catch(err => {
+      console.error(err)
+    })
+})
