1831f65d3f9fb552f9ae82ebdd3aa7c19d6b8652
Khaled Osman <khaled.osmaan@gmail.com>
2019-09-27 13:25:08 +0200

    perf: better import for sqs instead of entire aws-sdk

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 'use strict'
 const express = require('express')
 require('dotenv').config()
-const AWS = require('aws-sdk')
-AWS.config.update({
+const SQS = require('aws-sdk/clients/sqs')
+
+const sqs = new SQS({
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_KEY,
   region: process.env.AWS_REGION
 })
-const sqs = new AWS.SQS()
 
 const params = {
   MessageBody: 'Hello From Data Importer',
